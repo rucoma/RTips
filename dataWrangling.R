4f390351d647b3b674b1d70d4cabf379c51eeca4
rucoma <ruben.coca@mail.com>
2017-11-30 23:22:14 +0100

    x

--- dataWrangling.R
+++ dataWrangling.R
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-##########################
-# Created by: rucoma     #
-# Date:       2017-11-30 #
-##########################
-
-library(tidyverse)
-library(data.table)
-
-## Get the last/first row from a grouped data frame / data.table
-# Data.table approach
-datos[order(idUnico, fecha)][, lapply(.SD,tail,1), by = idUnico]
-#tidyverse approach (using a function to reuse)
-=======
 ##########################
 # Created by: rucoma     #
 # Date:       2017-11-30 #
@@ -34,5 +20,3 @@ lastRowGroup <- function(df, fieldId, fieldDate) {
     do(tail(., 1))
   return(df)
 }
-
->>>>>>> bebc8fca585fa1935cef656e03d8cde3108fb079

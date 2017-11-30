48ba302970d49a1ca7890a1c5f57bf9a697ef6d5
Rubén Coca <ruben.coca@gmail.com>
2017-11-30 18:29:00 +0100

    Update dataWrangling.R

--- dataWrangling.R
+++ dataWrangling.R
@@ -7,6 +7,17 @@ library(tidyverse)
 library(data.table)
 
 ## Get the last/first row from a grouped data frame / data.table
+
 # Data.table approach
 datos[order(idUnico, fecha)][, lapply(.SD,tail,1), by = idUnico]
+
 #tidyverse approach (using a function to reuse)
+lastRowGroup <- function(df, fieldId, fieldDate) {
+  library(tidyverse)
+  df <- df %>%
+    group_by_(fieldId) %>%
+    arrange_(fieldId, fieldDate) %>%
+    do(tail(., 1))
+  return(df)
+}
+

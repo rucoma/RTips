159085da56e8c5014ff92f48337702c0f12b3996
Rubén Coca <ruben.coca@gmail.com>
2017-12-01 16:15:13 +0100

    added new tips

--- dataWrangling.R
+++ dataWrangling.R
@@ -6,12 +6,15 @@
 library(tidyverse)
 library(data.table)
 
-## Get the last/first row from a grouped data frame / data.table
+## Get the last/first row from a grouped data frame / data.table -
 
 # Data.table approach
-datos[order(idUnico, fecha)][, lapply(.SD,tail,1), by = idUnico]
+datos[order(idUnico, fecha)][, lapply(.SD, tail, 1), by = idUnico]
 
-#tidyverse approach (using a function to reuse)
+# Another way using data.table
+dtClean <- dt[, tail(.SD, 1), by = c('var1', 'var2', 'var3')]
+
+# tidyverse approach (using a function to reuse)
 lastRowGroup <- function(df, fieldId, fieldDate) {
   library(tidyverse)
   df <- df %>%
@@ -20,3 +23,26 @@ lastRowGroup <- function(df, fieldId, fieldDate) {
     do(tail(., 1))
   return(df)
 }
+
+## Substitute to ifelse() -> dplyr::case_when() -----------------
+case_when((str_sub(string = Telefono, start = 1, end = 2) != '34') | (nchar(Telefono) < 9) ~ paste0('34', Telefono), 
+          TRUE ~Telefono)
+
+## Creating labelled intervals ----------------------------------
+cut(df$var,
+    breaks = c(-Inf, 50000, 500000, 1000000, Inf),
+    labels = c("<50MWh", "Entre 50MWh y 500MWh", "Entre 500MWh y 1GWh", "Mas 1GWh"))
+
+## Get tne name of a variable as string -------------------------
+name <- deparse(substitute(p48Final))
+
+## Change the name of the variable based in some criteria -------
+assign(name, df)
+
+## Load some files to the environment ---------------------------
+lapply(objects, load, .GlobalEnv)
+
+## Sum columns according some criteria --------------------------
+colSum <- c("col1", "col2", "col3")
+df$varSum <- rowSums(df[, colSum, with = F], na.rm = T)
+

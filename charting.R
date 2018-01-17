873b64fedd225901b63f97fbaa018e48a5adc321
Rubén Coca <ruben.coca@gmail.com>
2018-01-17 18:20:42 +0100

    Update charting.R

--- charting.R
+++ charting.R
@@ -5,3 +5,17 @@
 
 library(ggplot2)
 library(plotly)
+
+## Using data frame variables as arguments in custom function
+testplot <- function(meansdf, xvar, yvar, fillvar) {
+  ggplot(meansdf,
+         aes_string(x = xvar, 
+                    y= yvar, 
+                    color = fillvar)) +
+    geom_point(alpha = 0.2) +
+    geom_smooth(method = 'loess',
+                se = F) +
+    facet_wrap(~ Species)
+}
+
+testplot(meansdf = iris, xvar = 'Sepal.Length', yvar = 'Petal.Length', fillvar = 'Species')

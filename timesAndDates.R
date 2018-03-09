43e1ae6a7e891d9d84c3e2588c9951a0d316ed0d
Rubén Coca <ruben.coca@gmail.com>
2018-03-09 13:38:17 +0100

    Update timesAndDates.R

--- timesAndDates.R
+++ timesAndDates.R
@@ -6,4 +6,5 @@
 library(lubridate)
 library(zoo)
 
-
+# Convert to date excel dates (numbers) ---------------------------------------
+dt[, (colDate) := lapply(.SD, as.Date, origin = '1899-12-30'), .SDcols = colDate]

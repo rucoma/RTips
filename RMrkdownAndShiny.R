2a9956e20708a1d29fd0a09878e1cb833c575b1f
Rubén Coca <ruben.coca@gmail.com>
2018-03-09 09:08:36 +0100

    Update RMrkdownAndShiny.R

--- RMrkdownAndShiny.R
+++ RMrkdownAndShiny.R
@@ -10,13 +10,13 @@ runApp('./appCurso')
 
 ## Plotly charts side by side in RMarkdown ------
 <div>
- <div style = "float:left">
+ <div style = "display:inline-block">
  ```{r echo = F, fig.width = 4, fig.height = 4}
  p
  ```
  </div>
 
- <div style = "float:right">
+ <div style = "display:inline-block">
  ```{r, echo = F, fig.width = 4, fig.height = 4}
  p
  ```
